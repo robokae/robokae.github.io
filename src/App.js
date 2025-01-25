a501bba87d48dd9df3eb3fa2e95158898fcdb44d
Alexander Hom <alexanderhom19@gmail.com>
2025-01-25 13:23:44 -0800

    Remove unused routes

--- src/App.js
+++ src/App.js
@@ -12,15 +12,6 @@ import { GlobalStyles } from "./global/globalStyles";
 import { lightTheme, darkTheme } from "./global/theme";
 
 import Home from "./pages/home/Home";
-import Blog from "./pages/blog/Blog";
-import Post from "./pages/blog/Post";
-import Portfolio from "./pages/portfolio/Portfolio";
-import Resume from "./pages/resume/Resume";
-import Login from "./pages/Login";
-
-import Admin from "./pages/admin/Admin";
-import CreatePost from "./pages/admin/CreatePost";
-import EditPost from "./pages/admin/EditPost";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
@@ -63,14 +54,6 @@ function App() {
           <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/post/:postTitle" element={<Post />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/admin/createPost" element={<CreatePost />} />
-          <Route path="/admin/editPost" element={<EditPost />} />
           <Route path="/*" element={<Error />} />
         </Route>
       </>
