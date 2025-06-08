e3cfa76cdabbc51a64fd0c36184b37b715b5ce24
Alexander Hom <alexanderhom19@gmail.com>
2025-06-08 13:18:50 -0700

    Update useTheme hook

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -1,12 +1,17 @@
 import { Style } from "constants/style";
 import { darkTheme, lightTheme } from "global/theme";
-import { useEffect, useState } from "react";
-
-const THEME = "theme";
+import { useState } from "react";
 
 const useTheme = () => {
-  const [theme, setTheme] = useState(Style.DARK_THEME);
-  const [styles, setStyles] = useState(darkTheme);
+  const THEME = "theme";
+  const fetchSavedTheme = () => {
+    return localStorage.getItem(THEME);
+  };
+
+  const [theme, setTheme] = useState(fetchSavedTheme() || Style.DARK_THEME);
+  const [styles, setStyles] = useState(
+    fetchSavedTheme() === Style.LIGHT_THEME ? lightTheme : darkTheme
+  );
 
   const updateThemeValue = (newTheme) => {
     setTheme(newTheme);
@@ -19,11 +24,6 @@ const useTheme = () => {
       ? updateThemeValue(Style.DARK_THEME)
       : updateThemeValue(Style.LIGHT_THEME);
   };
-
-  useEffect(() => {
-    const persistedTheme = localStorage.getItem(THEME);
-    persistedTheme && setTheme(persistedTheme);
-  }, []);
 
   return { theme, styles, toggleTheme };
 };
